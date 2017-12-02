b869faf75a4c737726a0a7492d96997ebe0256b6
Candy <candy.espulgar@gmail.com>
2017-12-02 12:28:09 +0900

    Minor Changes

--- PROGLAN_RUBY.rb
+++ PROGLAN_RUBY.rb
@@ -2,7 +2,7 @@ BEGIN {
 	# Print a notice that the program has started
 	tab1 = "\t"
 	puts "\n[RUBY START]"
-	puts "\n"+tab1+"Range: 0 < INPUT < 10001\n"+tab1+"[STOP] ends the program.\n\n"
+	puts "\n"+tab1+"Range: 0 < INPUT < 10001\n"+tab1+"Type [STOP] to end the program\n\n"
 }
 
 END {
@@ -13,7 +13,7 @@ END {
 
 # Loop while the user doesn't type STOP
 loop do 
-	
+
 	# Ask for user input
 	print tab1+"Input:"+tab1
 	user_input = gets.chomp
