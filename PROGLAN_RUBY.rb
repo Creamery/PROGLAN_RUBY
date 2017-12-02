b7369e4645a315691eca6538d92d2373a27c59f2
Candy <candy.espulgar@gmail.com>
2017-12-02 11:20:33 +0900

    New Algorithm

--- PROGLAN_RUBY.rb
+++ PROGLAN_RUBY.rb
@@ -4,16 +4,28 @@ BEGIN {
 
 END {
 	puts "[RUBY END]"
-
 }
 
-print "Number of PRINT statements: "
 
-user_input = gets.chomp.to_i
-user_input = Math.log2(user_input).ceil
+user_input = 1;
+condition = user_input != "stop" && user_input.to_i > 0
 
-puts "Output: #{user_input}"
+while condition
+	print "Input: "
 
+	user_input = gets.chomp
+	if !condition
+		input = user_input.to_i
+		exponent = -1
+		product = 0
 
+		while product < input
+			exponent += 1
+			product = 2**exponent
+		end
 
-
+		actual = Math.log2(input).ceil
+		puts "Output: #{exponent}"
+		puts "Actual: #{actual}"
+	end
+end
